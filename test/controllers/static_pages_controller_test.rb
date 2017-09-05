f382848cdd15d77a12a4a74ddf18a7c900964c39
Shilpi Sharma <sap.developer.shilpi@gmail.com>
2017-09-05 15:06:00 +0200

    Finish Static pages

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title" , "Home | Library App"
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+    assert_select "title" , "Help | Library App"
+  end
+  test "should get about" do
+    get static_pages_about_url
+    assert_respond  :success
+    assert_select "title" , "About | Library App"
+  end
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_respond :success
+    assert_select "title" , "Contact | Library App"
+  end
+
+end
